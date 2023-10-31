6333d867726193685bd142548d51bae7b829b8b7
Daniil Safonov <dani@Daniils-MacBook-Air-2.local>
2023-10-31 12:18:34 +0000

    ui: fix spacing

--- app/src/main/java/com/example/vocabulario/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/vocabulario/ui/screens/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.text.KeyboardActions
@@ -73,6 +74,7 @@ fun MainScreen(
                 onDone = { onAddWordSubmit() }
             )
         )
+        Spacer(modifier = Modifier.height(16.dp))
         Column {
             Button(
                 onClick = onPracticeClick,
